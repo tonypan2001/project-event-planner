f1cab2aee38816068a7f541d09c03ba7424406ff
Prompan Uechanwech <panzaza9543@gmail.com>
2023-08-11 14:34:02 +0700

    merge main

--- resources/views/event/index.blade.php
+++ resources/views/event/index.blade.php
@@ -51,18 +51,54 @@
 
         <div class="border-b border-gray-400 mt-6"></div>
 
-        <div class="grid grid-cols-3 my-5">
-          <h1 class="text-4xl font-bold">Jogging Event</h1>
-          <div>
-            <h3 class="text-lg font-medium">Detail</h3>
-            <p class="text-base">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae illo officiis consectetur beatae vero? Assumenda doloribus vero atque facere tempora reiciendis repudiandae quasi, dolor quae itaque! Culpa cum debitis nulla!</p>
-          </div>
+        <div class="grid grid-cols-1 my-5">
+            <div class="flex flex-row justify-center items-center py-2 my-2">
+                <h1 class="text-4xl font-bold text-center w-2/3">Jogging Event</h1>
+                <div class="flex justify-end items-center w-1/3">
+                    <a class="flex justify-center items-center w-32 bg-white border border-2 border-mypurple-light hover:border-mypurple-dark text-mypurple-light hover:text-mypurple-dark text-center cursor-pointer font-bold py-2 px-4 rounded-full">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-gear" viewBox="0 0 16 16">
+                            <path d="M8 4.754a3.246 3.246 0 1 0 0 6.492 3.246 3.246 0 0 0 0-6.492zM5.754 8a2.246 2.246 0 1 1 4.492 0 2.246 2.246 0 0 1-4.492 0z"/>
+                            <path d="M9.796 1.343c-.527-1.79-3.065-1.79-3.592 0l-.094.319a.873.873 0 0 1-1.255.52l-.292-.16c-1.64-.892-3.433.902-2.54 2.541l.159.292a.873.873 0 0 1-.52 1.255l-.319.094c-1.79.527-1.79 3.065 0 3.592l.319.094a.873.873 0 0 1 .52 1.255l-.16.292c-.892 1.64.901 3.434 2.541 2.54l.292-.159a.873.873 0 0 1 1.255.52l.094.319c.527 1.79 3.065 1.79 3.592 0l.094-.319a.873.873 0 0 1 1.255-.52l.292.16c1.64.893 3.434-.902 2.54-2.541l-.159-.292a.873.873 0 0 1 .52-1.255l.319-.094c1.79-.527 1.79-3.065 0-3.592l-.319-.094a.873.873 0 0 1-.52-1.255l.16-.292c.893-1.64-.902-3.433-2.541-2.54l-.292.159a.873.873 0 0 1-1.255-.52l-.094-.319zm-2.633.283c.246-.835 1.428-.835 1.674 0l.094.319a1.873 1.873 0 0 0 2.693 1.115l.291-.16c.764-.415 1.6.42 1.184 1.185l-.159.292a1.873 1.873 0 0 0 1.116 2.692l.318.094c.835.246.835 1.428 0 1.674l-.319.094a1.873 1.873 0 0 0-1.115 2.693l.16.291c.415.764-.42 1.6-1.185 1.184l-.291-.159a1.873 1.873 0 0 0-2.693 1.116l-.094.318c-.246.835-1.428.835-1.674 0l-.094-.319a1.873 1.873 0 0 0-2.692-1.115l-.292.16c-.764.415-1.6-.42-1.184-1.185l.159-.291A1.873 1.873 0 0 0 1.945 8.93l-.319-.094c-.835-.246-.835-1.428 0-1.674l.319-.094A1.873 1.873 0 0 0 3.06 4.377l-.16-.292c-.415-.764.42-1.6 1.185-1.184l.292.159a1.873 1.873 0 0 0 2.692-1.115l.094-.319z"/>
+                          </svg>
+                          <p class="mx-2">
+                              Manage
+                          </p>
+                    </a>
+                </div>
+            </div>
+
+            <div class="grid grid-rows gap-6 mx-4">
+                <div>
+                    <h3 class="text-lg font-bold">About Event</h3>
+                    <p class="text-base">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae illo officiis consectetur beatae vero? Assumenda doloribus vero atque facere tempora reiciendis repudiandae quasi, dolor quae itaque! Culpa cum debitis nulla!</p>
+                </div>
+                <div>
+                    <h3 class="text-lg font-bold">Property</h3>
+                    <p class="text-base">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae illo officiis consectetur beatae vero? Assumenda doloribus vero atque facere tempora reiciendis repudiandae quasi, dolor quae itaque! Culpa cum debitis nulla!</p>
+                </div>
+            </div>
         </div>
 
-        <div class="flex content-center justify-between w-full mb-5 mx-6 mt-5">
-            <a href="{{route('dashboard.index')}}" class="w-full">< Back</a>
-            <button class="w-full bg-mypink-light hover:bg-mypink-dark text-white font-bold py-2 px-10 rounded-full">Join Event</button>
-            <div class="w-full"></div>
+        <div class="border-b border-gray-400 mt-6"></div>
+
+        <div class="flex content-center justify-between w-full mb-5 mx-6 mt-5 p-4">
+            <div class="w-2/3 flex justify-center items-center">
+                <a href="{{route('dashboard.index')}}" class="w-full">< Back</a>
+            </div>
+            <div class="w-1/3 flex justify-center items-center">
+                <a class="w-full bg-mypink-light hover:bg-mypink-dark text-white text-center cursor-pointer font-bold py-2 px-10 rounded-full">Join Event</a>
+            </div>
+            <div class="w-1/4 flex justify-center items-center">
+                <a class="flex justify-center items-center w-32 bg-white border border-2 border-mypink-light hover:border-mypink-dark text-mypink-light hover:text-mypink-dark text-center cursor-pointer font-bold py-2 px-4 rounded-full">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-pencil-square" viewBox="0 0 16 16">
+                        <path d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z"/>
+                        <path fill-rule="evenodd" d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5v11z"/>
+                      </svg>
+                      <p class="mx-2">
+                          Kanban
+                      </p>
+                </a>
+            </div>
         </div>
     </div>
 </div> 
