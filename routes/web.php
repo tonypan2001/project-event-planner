55f26dde4c2cf57112d89eb35885fa07e8fa95b4
beam7894123 <susit.t@ku.th>
2023-08-19 03:54:09 +0700

    connect login.blade.php to database & Reroute in web.php and RouteServiceProvider.php

--- routes/web.php
+++ routes/web.php
@@ -20,12 +20,11 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-// Route Login
-Route::get('/', function () {
+// Go to login first anon! >:<
+Route::get('/signin', function () {
     return view('auth.login');
 })->name('login');
-
-Route::get('/signin', function () {
+Route::get('/', function () {
     return view('auth.login');
 })->name('login');
 
@@ -34,24 +33,43 @@ Route::get('/register', function () {
     return view('auth.register');
 })->name('register');
 
-// Route Dashboard
-Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard.index');
+// Dont want anon to get to ur web without login?
+// Use This!
+Route::middleware(['auth'])->group(function () {
 
-Route::get('/notification', [NotificationController::class, 'index'])->name('notification.index');
+    Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard.index');
 
-Route::get('/gallery', [GalleryController::class, 'index'])->name('gallery.index');
+    Route::get('/notification', [NotificationController::class, 'index'])->name('notification.index');
 
-Route::get('/settings', [SettingsController::class, 'index'])->name('settings.index');
+    Route::get('/gallery', [GalleryController::class, 'index'])->name('gallery.index');
+
+    Route::get('/settings', [SettingsController::class, 'index'])->name('settings.index');
 
 // Event
-Route::get('/event', [EventController::class, 'index'])->name('event.index');
-Route::get('/create-event', [EventController::class, 'create'])->name('event.create');
-Route::get('/event/manage', [EventController::class, 'manager'])->name('event.manage');
-Route::get('/event/edit', [EventController::class, 'edit'])->name('event.edit');
-Route::get('/event/manage/edit/budget', [EventController::class, 'editBudget'])->name('event.editBudget');
-Route::get('/event/manage/edit/worker', [EventController::class, 'editWorker'])->name('event.editWorker');
+    Route::get('/event', [EventController::class, 'index'])->name('event.index');
+    Route::get('/create-event', [EventController::class, 'create'])->name('event.create');
+    Route::get('/event/manage', [EventController::class, 'manager'])->name('event.manage');
+    Route::get('/event/edit', [EventController::class, 'edit'])->name('event.edit');
+    Route::get('/event/manage/edit/budget', [EventController::class, 'editBudget'])->name('event.editBudget');
+    Route::get('/event/manage/edit/worker', [EventController::class, 'editWorker'])->name('event.editWorker');
 
 // Whiteboard
-Route::get('/event/whiteboard', [WhiteboardController::class, 'index'])->name('event.whiteboard');
-Route::post('/event/whiteboard', [WhiteboardController::class, 'store'])->name('event.store');
-Route::delete('/event/whiteboard', [WhiteboardController::class, 'destroy'])->name('event.destroy');
+    Route::get('/event/whiteboard', [WhiteboardController::class, 'index'])->name('event.whiteboard');
+    Route::post('/event/whiteboard', [WhiteboardController::class, 'store'])->name('event.store');
+    Route::delete('/event/whiteboard', [WhiteboardController::class, 'destroy'])->name('event.destroy');
+});
+require __DIR__.'/auth.php';
+
+//Route::get('/dashboard', function () {
+//    return view('dashboard');
+//})->middleware(['auth', 'verified'])->name('dashboard');
+
+//Route::middleware('auth')->group(function () {
+//    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+//    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+//    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+//});
+
+// OLD Route Dashboard
+//Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard.index');
+
