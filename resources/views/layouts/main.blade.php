5571895792f6bc63ab36fb652c12aa0e809aca2e
beam7894123 <susit.t@ku.th>
2023-08-19 01:09:37 +0700

    LogOut Link are now usable

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -53,12 +53,34 @@
                         </svg>
                         <p class="mx-2">Settings</p>
                     </a>
-                    <a href="{{route('login')}}" class="flex flex-row justify-center items-center mt-4 p-2 font-mediu border border-2 rounded-full w-full hover:border-mypink-light hover:text-mypink-light">
+
+                    {{--LogOut Link--}}
+                    <a id="logoutLink" href="#" onclick="logout()" class="flex flex-row justify-center items-center mt-4 p-2 font-mediu border border-2 rounded-full w-full hover:border-mypink-light hover:text-mypink-light">
                         <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-box-arrow-left" viewBox="0 0 16 16">
                             <path fill-rule="evenodd" d="M6 12.5a.5.5 0 0 0 .5.5h8a.5.5 0 0 0 .5-.5v-9a.5.5 0 0 0-.5-.5h-8a.5.5 0 0 0-.5.5v2a.5.5 0 0 1-1 0v-2A1.5 1.5 0 0 1 6.5 2h8A1.5 1.5 0 0 1 16 3.5v9a1.5 1.5 0 0 1-1.5 1.5h-8A1.5 1.5 0 0 1 5 12.5v-2a.5.5 0 0 1 1 0v2z"/>
                             <path fill-rule="evenodd" d="M.146 8.354a.5.5 0 0 1 0-.708l3-3a.5.5 0 1 1 .708.708L1.707 7.5H10.5a.5.5 0 0 1 0 1H1.707l2.147 2.146a.5.5 0 0 1-.708.708l-3-3z"/>
                         </svg>
                         <p class="mx-2">Log Out</p>
+                        {{-- script POST for <a> logout
+                            dont ask how I do it lol --}}
+                        <script>
+                            function logout() {
+                                var form = document.createElement('form');
+                                form.method = 'POST';
+                                form.action = '{{ route('logout') }}';
+                                form.style.display = 'none';
+
+                                {{--just like @csrf--}}
+                                var csrfToken = document.createElement('input');
+                                csrfToken.setAttribute('type', 'hidden');
+                                csrfToken.setAttribute('name', '_token');
+                                csrfToken.setAttribute('value', '{{ csrf_token() }}');
+                                form.appendChild(csrfToken);
+
+                                document.body.appendChild(form);
+                                form.submit();
+                            }
+                        </script>
                     </a>
                 </div>
             </div>
