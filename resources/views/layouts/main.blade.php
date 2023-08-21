760a7aa2dff10d60a1bc09265da0545cc5183ef6
beam7894123 <susit.t@ku.th>
2023-08-21 23:15:55 +0700

    Remove Notifications

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -45,12 +45,12 @@
                         </svg>
                         <p class="mx-2">Dashboard</p>
                     </a>
-                    <a href="{{route('notification.index')}}" class="flex flex-row justify-center items-center mt-4 p-2 font-mediu border border-2 rounded-full w-full hover:border-mypink-light hover:text-mypink-light">
-                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-bell" viewBox="0 0 16 16">
-                            <path d="M8 16a2 2 0 0 0 2-2H6a2 2 0 0 0 2 2zM8 1.918l-.797.161A4.002 4.002 0 0 0 4 6c0 .628-.134 2.197-.459 3.742-.16.767-.376 1.566-.663 2.258h10.244c-.287-.692-.502-1.49-.663-2.258C12.134 8.197 12 6.628 12 6a4.002 4.002 0 0 0-3.203-3.92L8 1.917zM14.22 12c.223.447.481.801.78 1H1c.299-.199.557-.553.78-1C2.68 10.2 3 6.88 3 6c0-2.42 1.72-4.44 4.005-4.901a1 1 0 1 1 1.99 0A5.002 5.002 0 0 1 13 6c0 .88.32 4.2 1.22 6z"/>
-                        </svg>
-                        <p class="mx-2">Notifications</p>
-                    </a>
+{{--                    <a href="{{route('notification.index')}}" class="flex flex-row justify-center items-center mt-4 p-2 font-mediu border border-2 rounded-full w-full hover:border-mypink-light hover:text-mypink-light">--}}
+{{--                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-bell" viewBox="0 0 16 16">--}}
+{{--                            <path d="M8 16a2 2 0 0 0 2-2H6a2 2 0 0 0 2 2zM8 1.918l-.797.161A4.002 4.002 0 0 0 4 6c0 .628-.134 2.197-.459 3.742-.16.767-.376 1.566-.663 2.258h10.244c-.287-.692-.502-1.49-.663-2.258C12.134 8.197 12 6.628 12 6a4.002 4.002 0 0 0-3.203-3.92L8 1.917zM14.22 12c.223.447.481.801.78 1H1c.299-.199.557-.553.78-1C2.68 10.2 3 6.88 3 6c0-2.42 1.72-4.44 4.005-4.901a1 1 0 1 1 1.99 0A5.002 5.002 0 0 1 13 6c0 .88.32 4.2 1.22 6z"/>--}}
+{{--                        </svg>--}}
+{{--                        <p class="mx-2">Notifications</p>--}}
+{{--                    </a>--}}
                     <a href="{{route('gallery.index')}}" class="flex flex-row justify-center items-center mt-4 p-2 font-mediu border border-2 rounded-full w-full hover:border-mypink-light hover:text-mypink-light">
                         <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-images" viewBox="0 0 16 16">
                             <path d="M4.502 9a1.5 1.5 0 1 0 0-3 1.5 1.5 0 0 0 0 3z"/>
